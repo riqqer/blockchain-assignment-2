9448303516af48abab7cd8b672df445eac51827f
wstyx-hh <nora.naomivna@gmail.com>
2025-04-14 07:53:27 +0500

    Initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,36 @@ pub fn process_instruction(
     _accounts: &[AccountInfo],
     _instruction_data: &[u8],
 ) -> ProgramResult {
-    msg!("Hello, world!");
+    msg!("Hello, Solana!");
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use solana_program_test::*;
+    use solana_sdk::{
+        instruction::Instruction, pubkey::Pubkey, signature::Signer, transaction::Transaction,
+    };
+
+    #[tokio::test]
+    async fn test_hello_world() {
+        let program_id = Pubkey::new_unique();
+        let mut program_test = ProgramTest::default();
+        program_test.add_program("hello_world", program_id, None);
+        let (mut banks_client, payer, recent_blockhash) = program_test.start().await;
+        // Create instruction
+        let instruction = Instruction {
+            program_id,
+            accounts: vec![],
+            data: vec![],
+        };
+        // Create transaction with instruction
+        let mut transaction = Transaction::new_with_payer(&[instruction], Some(&payer.pubkey()));
+
+        // Sign transaction
+        transaction.sign(&[&payer], recent_blockhash);
+
+        let transaction_result = banks_client.process_transaction(transaction).await;
+        assert!(transaction_result.is_ok());
+    }
 }
